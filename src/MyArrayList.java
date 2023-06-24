9256d8161b7d81505662a8d734763db8689e94a5
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2023-06-24 08:48:09 +0200

    upd arrayList

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,53 +1,60 @@
 import java.util.Arrays;
 
 public class MyArrayList<T> {
+	private static final int INITIAL_CAPACITY = 15;
+	private static final int CAPACITY_INCREMENT = 15;
+
 	private T[] array;
+	private int size;
 
 	public MyArrayList() {
-		array = (T[]) new Object[0];
+		array = (T[]) new Object[INITIAL_CAPACITY];
+		size = 0;
 	}
 
 	public void add(T value) {
-		T[] temp = Arrays.copyOf(array, array.length + 1);
-		temp[temp.length - 1] = value;
-		array = temp;
+		if (size == array.length) {
+			expandArray();
+		}
+		array[size] = value;
+		size++;
 	}
 
 	public void remove(int index) {
-		if (index < 0 || index >= array.length) {
+		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Invalid index");
 		}
 
-		T[] temp = (T[]) new Object[array.length - 1];
-		int tempIndex = 0;
-
-		for (int i = 0; i < array.length; i++) {
-			if (i != index) {
-				temp[tempIndex] = array[i];
-				tempIndex++;
-			}
+		for (int i = index; i < size - 1; i++) {
+			array[i] = array[i + 1];
 		}
 
-		array = temp;
+		array[size - 1] = null;
+		size--;
 	}
 
 	public void clear() {
-		array = (T[]) new Object[0];
+		Arrays.fill(array, null);
+		size = 0;
 	}
 
 	public int size() {
-		return array.length;
+		return size;
 	}
 
 	@Override
 	public String toString() {
-		return Arrays.toString(array);
+		return Arrays.toString(Arrays.copyOf(array, size));
 	}
 
 	public T get(int index) {
-		if (index < 0 || index >= array.length) {
+		if (index < 0 || index >= size) {
 			throw new IndexOutOfBoundsException("Invalid index");
 		}
 		return array[index];
+	}
+
+	private void expandArray() {
+		array = Arrays.copyOf(array, array.length + CAPACITY_INCREMENT);
 	}
 }
